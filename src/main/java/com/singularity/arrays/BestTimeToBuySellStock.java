0c2b6bf57f099cccdb206d7a61b3c299c64bd94f
Guilherme Herter <nogr.gh@gmail.com>
2025-12-30 12:29:56 -0300

    Adding solution and tests

---
+++ src/main/java/com/singularity/arrays/BestTimeToBuySellStock.java
@@ -0,0 +1,19 @@
+package com.singularity.arrays;
+
+public class BestTimeToBuySellStock {
+
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+
+        for (int price : prices) {
+            if (price < min) {
+                min = price;
+            } else {
+                max = Math.max(max, price - min);
+            }
+        }
+
+        return max;
+    }
+}
